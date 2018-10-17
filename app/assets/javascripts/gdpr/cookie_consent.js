189dd56ced0a7150f8810694cb86c5ac3077b3c6
David Reese <david@whatcould.com>
2018-10-17 12:59:14 -0400

    Attach click handler to document
    Handles turbolinks

--- app/assets/javascripts/gdpr/cookie_consent.js
+++ app/assets/javascripts/gdpr/cookie_consent.js
@@ -1,6 +1,6 @@
 //= require js.cookie
 $(function(){
-  $('.js-gdpr__cookie_consent__buttons__ok').click(function() {
+  $(document).on('click', '.js-gdpr__cookie_consent__buttons__ok', function() {
     Cookies.set('gdpr.cookie_consent.ok', true, { path: '/', expires: 365 });
     $('.js-gdpr__cookie_consent').remove();
   });
